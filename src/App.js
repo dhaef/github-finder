b0a87e301f7f0b701e56805118f9d12173cea13f
Derek <derekhaefner@gmail.com>
2020-03-17 20:21:53 -0400

    add inital files

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import Navbar from './component/layout/Navbar';
+import Users from './component/users/Users';
+import axios from 'axios';
+import './App.css';
+
+class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  }
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+
+    const res = await axios.get('https://api.github.com/users');
+
+    this.setState({ users: res.data, loading: false });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <div id='container'>
+          <Users loading={this.state.loading} users={this.state.users}/>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
